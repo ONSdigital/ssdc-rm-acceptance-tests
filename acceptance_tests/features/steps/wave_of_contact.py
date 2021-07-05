73b399c12b28c514dbd70cb5748df8523e7b0ffc
AdamHawtin <adam.hawtin@ons.gov.uk>
2021-07-05 07:52:25 +0100

    Remove context.classifiers

--- acceptance_tests/features/steps/wave_of_contact.py
+++ acceptance_tests/features/steps/wave_of_contact.py
@@ -6,5 +6,4 @@ from acceptance_tests.utilities.wave_of_contact_helper import create_print_wave_
 @step('a print wave of contact has been created with template "{template}" and classifiers "{classifiers}"')
 def create_print_wave_of_contact_with_template_and_classifers(context, template, classifiers):
     context.template = template
-    context.classifiers = classifiers
     context.pack_code = create_print_wave_of_contact(context.collex_id, classifiers, template)
