10151acdf958645ba5d883b5a703369827880fbb
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-07-06 11:59:07 +0100

    Fix merge hell

--- acceptance_tests/features/steps/action_rule.py
+++ acceptance_tests/features/steps/action_rule.py
@@ -5,8 +5,13 @@ from acceptance_tests.utilities.action_rule_helper import create_print_action_ru
 
 
 @step('a print action rule has been created with classifiers "{classifiers}"')
-def create_print_action_rule_with_template_and_classifiers(context, classifiers):
+def create_print_action_rule_with_classifiers(context, classifiers):
     create_print_action_rule(context.collex_id, classifiers, context.pack_code)
+
+
+@step('a print action rule has been created')
+def create_print_action_rule_no_classifiers(context):
+    create_print_action_rule(context.collex_id, '', context.pack_code)
 
 
 @step('a deactivate uac action rule has been created')
