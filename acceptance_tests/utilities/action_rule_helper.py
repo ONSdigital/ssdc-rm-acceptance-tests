5945b81eb8fecd5fde5e0f6a392ebb4a6f6ca3a9
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-07-06 09:44:47 +0100

    Revert "Merge hell"
    This reverts commit 84b21333fc6b364cfe4fb5198b5efc2391c50e41.

--- acceptance_tests/utilities/action_rule_helper.py
+++ acceptance_tests/utilities/action_rule_helper.py
@@ -1,3 +1,6 @@
+import json
+import random
+import string
 import uuid
 from datetime import datetime
 
@@ -6,31 +9,25 @@ import requests
 from config import Config
 
 
-def create_print_action_rule(collex_id, classifiers, pack_code):
+def create_action_rule(context):
+    # whilst action rules are created to get a UAC for example to receipt, a printfile will still be created after
+    # that test has finished, this interferes with other tests as the printfile timestamps is often after the start
+    # of the next test.
+    # By using a unique random pack_code we have better filter options
+    # We can change/remove this if we get UACS differently or a better solution is found
+    context.pack_code = 'pack_code_' + ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+
     url = f'{Config.SUPPORT_TOOL}/actionRules'
     body = {
         'id': str(uuid.uuid4()),
         'type': 'PRINT',
-        'printTemplate': '/printTemplates/' + pack_code,
+        'packCode': context.pack_code,
         'triggerDateTime': f'{datetime.utcnow().isoformat()}Z',
         'hasTriggered': False,
-        'classifiers': classifiers,
-        'collectionExercise': 'collectionExercises/' + collex_id
-    }
-
-    response = requests.post(url, json=body)
-    response.raise_for_status()
-
-
-def setup_deactivate_uac_action_rule(collex_id):
-    url = f'{Config.SUPPORT_TOOL}/actionRules'
-    body = {
-        'id': str(uuid.uuid4()),
-        'type': 'DEACTIVATE_UAC',
-        'triggerDateTime': f'{datetime.utcnow().isoformat()}Z',
-        'hasTriggered': False,
-        'classifiers': '',
-        'collectionExercise': 'collectionExercises/' + collex_id
+        'classifiers': context.classifiers,
+        'template': json.loads(context.template),
+        'printSupplier': 'SUPPLIER_A',
+        'collectionExercise':  'collectionExercises/' + context.collex_id
     }
 
     response = requests.post(url, json=body)
