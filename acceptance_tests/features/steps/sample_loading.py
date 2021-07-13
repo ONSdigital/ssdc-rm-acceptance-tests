7825960a61609142d3b684e52b1e5ad369e4f844
Leo Howard <38526889+LeoKHoward@users.noreply.github.com>
2021-07-13 12:05:27 +0100

    Generic Survey - Handling Sensitive Data (#10)
    * New test for sensitive data
    
    * Refactoring
    
    * Fix bug
    
    * AH suggested changes not timeout part
    
    * Retry added to database query
    
    * Suggested changes
    
    * Retry line readded to top of step
    
    Co-authored-by: Nick Grant <nick.grant@ext.ons.gov.uk>

--- acceptance_tests/features/steps/sample_loading.py
+++ acceptance_tests/features/steps/sample_loading.py
@@ -10,14 +10,14 @@ from acceptance_tests.utilities.validation_rule_helper import get_sample_rows_an
 from config import Config
 
 
-def get_emitted_cases_and_check_against_sample(sample_rows):
+def get_emitted_cases_and_check_against_sample(sample_rows, sensitive_columns=[]):
     emitted_cases = get_emitted_cases('CASE_CREATED', len(sample_rows))
 
     for emitted_case in emitted_cases:
         matched_row = None
         for sample_row in sample_rows:
 
-            if sample_row == emitted_case['sample']:
+            if equal_dicts(sample_row, emitted_case['sample'], sensitive_columns):
                 matched_row = sample_row
                 break
 
@@ -27,6 +27,12 @@ def get_emitted_cases_and_check_against_sample(sample_rows):
             test_helper.fail(f"Could not find matching row in the sample data for case: {emitted_case}")
 
     return emitted_cases
+
+
+def equal_dicts(d1, d2, ignore_keys):
+    d1_filtered = {k: v for k, v in d1.items() if k not in ignore_keys}
+    d2_filtered = {k: v for k, v in d2.items() if k not in ignore_keys}
+    return d1_filtered == d2_filtered
 
 
 @step('sample file "{sample_file_name}" is loaded successfully')
@@ -40,6 +46,19 @@ def load_sample_file_step(context, sample_file_name):
     upload_sample_file(context.collex_id, sample_file_path)
 
     context.emitted_cases = get_emitted_cases_and_check_against_sample(sample_rows)
+
+
+@step('sample file "{sample_file_name}" with sensitive column {sensitive_column} is loaded successfully')
+def load_sample_file_step_for_sensitive_data(context, sample_file_name, sensitive_column):
+    sample_file_path = Config.RESOURCE_FILE_PATH.joinpath('sample_files', sample_file_name)
+    sample_rows, sample_validation_rules = get_sample_rows_and_validation_rules(sample_file_path, sensitive_column)
+
+    context.survey_id = add_survey(sample_validation_rules)
+    context.collex_id = add_collex(context.survey_id)
+
+    upload_sample_file(context.collex_id, sample_file_path)
+
+    context.emitted_cases = get_emitted_cases_and_check_against_sample(sample_rows, {sensitive_column})
 
 
 def upload_sample_file(collex_id, sample_file_path):
---
+++ acceptance_tests/features/steps/sensitive_data.py
@@ -0,0 +1,43 @@
+import json
+
+from behave import step
+from tenacity import retry, wait_fixed, stop_after_delay
+
+from acceptance_tests.utilities.database_helper import open_write_cursor
+from acceptance_tests.utilities.rabbit_helper import publish_json_message
+from acceptance_tests.utilities.test_case_helper import test_helper
+from config import Config
+
+
+@step("an UPDATE_SAMPLE_SENSITIVE event is received updating the {sensitive_column}")
+def send_update_sample_sensitive_msg(context, sensitive_column):
+    message = json.dumps(
+        {
+            "event": {
+                "type": "UPDATE_SAMPLE_SENSITIVE",
+                "source": "RH",
+                "channel": "RH",
+                "dateTime": "2021-06-09T13:49:19.716761Z",
+                "transactionId": "92df974c-f03e-4519-8d55-05e9c0ecea43"
+            },
+            "payload": {
+                "updateSampleSensitive": {
+                    "caseId": context.emitted_cases[0]['caseId'],
+                    "sampleSensitive": {sensitive_column: "07898787878"}
+                }
+            }
+        })
+
+    publish_json_message(message, exchange=Config.RABBITMQ_EVENT_EXCHANGE,
+                         routing_key=Config.RABBITMQ_UPDATE_SAMPLE_SENSITIVE_ROUTING_KEY)
+
+
+@retry(wait=wait_fixed(1), stop=stop_after_delay(30))
+@step("the {sensitive_column} in the sensitive data on the case is updated")
+def sensitive_data_on_case_changed(context, sensitive_column):
+    with open_write_cursor() as cur:
+        cur.execute("SELECT sample_sensitive FROM casev3.cases WHERE id = %s", (context.emitted_cases[0]['caseId'],))
+        result = cur.fetchone()
+
+        test_helper.assertEqual(result[0]['PHONE_NUMBER'], '07898787878',
+                                "The phone number should have been updated, but it hasn't been")
