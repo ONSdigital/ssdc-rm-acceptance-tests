bed31b582c4bc4175a03212927646320070e1b2f
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2021-08-05 10:11:18 +0100

    Attempt to fix ATs by doing MEGA PURGE (#22)
    * Attempt to fix ATs by doing MEGA PURGE
    
    * Update environment.py

--- acceptance_tests/features/environment.py
+++ acceptance_tests/features/environment.py
@@ -41,10 +41,25 @@ def before_scenario(context, _):
 
 
 def after_all(_context):
+    # TODO - this is a hack and should be removed/refactored when we understand better what's going on
+    time.sleep(10)
+
     purge_queues()
+    purge_fulfilment_triggers()
+
+    # TODO - this is a hack and should be removed/refactored when we understand better what's going on
+    time.sleep(10)
 
 
 def after_scenario(_context, scenario):
+    # TODO - this is a hack and should be removed/refactored when we understand better what's going on
+    time.sleep(10)
+
+    purge_queues()
+
+    # TODO - this is a hack and should be removed/refactored when we understand better what's going on
+    time.sleep(10)
+
     if "clear_for_bad_messages" not in scenario.tags:
         response = requests.get(f'{Config.EXCEPTION_MANAGER_URL}/badmessages')
         response.raise_for_status()
