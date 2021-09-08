50914103150379d1fdfc16977ef915da08a649e3
Adam <35268982+AdamHawtin@users.noreply.github.com>
2021-09-08 12:17:50 +0100

    2754 Fix pubsub pull behaviour flakiness (#42)
    Co-authored-by: Nick Grant <nick.grant@ext.ons.gov.uk>

--- acceptance_tests/utilities/event_helper.py
+++ acceptance_tests/utilities/event_helper.py
@@ -1,19 +1,11 @@
-import json
-
-from google.cloud import pubsub_v1
-
+from acceptance_tests.utilities.pubsub_helper import get_exact_number_of_pubsub_messages
 from acceptance_tests.utilities.test_case_helper import test_helper
 from config import Config
 
 
 def get_emitted_cases(expected_msg_count=1):
-    messages_received = []
-    start_listening_to_pubsub_subscription(Config.PUBSUB_OUTBOUND_CASE_SUBSCRIPTION,
-                                           message_list=messages_received,
-                                           expected_msg_count=expected_msg_count)
-
-    test_helper.assertEqual(len(messages_received), expected_msg_count,
-                            'Did not find expected number of events')
+    messages_received = get_exact_number_of_pubsub_messages(Config.PUBSUB_OUTBOUND_CASE_SUBSCRIPTION,
+                                                            expected_msg_count=expected_msg_count)
 
     case_payloads = []
     for message_received in messages_received:
@@ -24,49 +16,9 @@ def get_emitted_cases(expected_msg_count=1):
     return case_payloads
 
 
-def start_listening_to_pubsub_subscription(subscription, expected_msg_count, message_list):
-    subscriber = pubsub_v1.SubscriberClient()
-    subscription_path = subscriber.subscription_path(Config.PUBSUB_PROJECT,
-                                                     subscription)
-    received_messages = _attempt_to_get_expected_number_of_messages(subscriber, subscription_path, expected_msg_count)
-
-    ack_ids = []
-
-    for received_message in received_messages:
-        parsed_body = json.loads(received_message.message.data)
-        message_list.append(parsed_body)
-        ack_ids.append(received_message.ack_id)
-
-    if ack_ids:
-        subscriber.acknowledge(subscription_path, ack_ids)
-
-    subscriber.close()
-
-
-def _attempt_to_get_expected_number_of_messages(subscriber, subscription_path, expected_msg_count):
-    messages = []
-    remaining_messages_to_get = expected_msg_count
-    last_one = False
-
-    while remaining_messages_to_get and not last_one:
-        if remaining_messages_to_get == 1:
-            last_one = True
-
-        response = subscriber.pull(subscription_path, max_messages=remaining_messages_to_get, timeout=30)
-
-        messages += response.received_messages
-        remaining_messages_to_get -= len(response.received_messages)
-
-    return messages
-
-
 def get_emitted_case_update(correlation_id, originating_user):
-    messages_received = []
-    start_listening_to_pubsub_subscription(Config.PUBSUB_OUTBOUND_CASE_SUBSCRIPTION,
-                                           message_list=messages_received,
-                                           expected_msg_count=1)
-
-    test_helper.assertEqual(len(messages_received), 1, 'Expected to receive one and only one CASE_UPDATE message')
+    messages_received = get_exact_number_of_pubsub_messages(Config.PUBSUB_OUTBOUND_CASE_SUBSCRIPTION,
+                                                            expected_msg_count=1)
 
     if correlation_id:
         test_helper.assertEqual(messages_received[0]['header']['correlationId'], correlation_id,
@@ -80,12 +32,8 @@ def get_emitted_case_update(correlation_id, originating_user):
 
 
 def get_emitted_uac_update(correlation_id, originating_user):
-    messages_received = []
-    start_listening_to_pubsub_subscription(Config.PUBSUB_OUTBOUND_UAC_SUBSCRIPTION,
-                                           message_list=messages_received,
-                                           expected_msg_count=1)
-
-    test_helper.assertEqual(len(messages_received), 1, 'Expected to receive one and only one UAC_UPDATE message')
+    messages_received = get_exact_number_of_pubsub_messages(Config.PUBSUB_OUTBOUND_UAC_SUBSCRIPTION,
+                                                            expected_msg_count=1)
 
     if correlation_id:
         test_helper.assertEqual(messages_received[0]['header']['correlationId'], correlation_id,
@@ -99,10 +47,10 @@ def get_emitted_uac_update(correlation_id, originating_user):
 
 
 def get_uac_update_events(expected_number, correlation_id, originating_user):
-    messages_received = []
-    start_listening_to_pubsub_subscription(Config.PUBSUB_OUTBOUND_UAC_SUBSCRIPTION,
-                                           message_list=messages_received,
-                                           expected_msg_count=expected_number)
+    messages_received = get_exact_number_of_pubsub_messages(Config.PUBSUB_OUTBOUND_UAC_SUBSCRIPTION,
+                                                            expected_msg_count=expected_number,
+                                                            timeout=60)
+
     uac_payloads = []
     for uac_event in messages_received:
         if correlation_id:
--- acceptance_tests/utilities/pubsub_helper.py
+++ acceptance_tests/utilities/pubsub_helper.py
@@ -1,9 +1,12 @@
+import json
 import logging
+import time
 
 from google.api_core.exceptions import DeadlineExceeded
 from google.cloud import pubsub_v1
 from structlog import wrap_logger
 
+from acceptance_tests.utilities.test_case_helper import test_helper
 from config import Config
 
 logger = wrap_logger(logging.getLogger(__name__))
@@ -53,10 +56,47 @@ def _ack_all_on_subscription(subscriber, subscription_path):
         return
 
     ack_ids = [message.ack_id for message in response.received_messages]
-
     if ack_ids:
         subscriber.acknowledge(subscription_path, ack_ids)
 
     # It's possible (though unlikely) that they could be > max_messages on the topic so keep deleting till empty
     if len(response.received_messages) == max_messages_per_attempt:
         _ack_all_on_subscription(subscriber, subscription_path)
+
+
+def _pull_exact_number_of_messages(subscriber, subscription_path, expected_msg_count, timeout):
+    # Synchronously pull messages one at at time until we either hit the expected number or the timeout passes.
+    received_messages = []
+    deadline = time.time() + timeout
+
+    # The PubSub subscriber client does not wait the full duration of its timeout before returning if it finds just
+    # at least one message. To work around this, we loop pulling messages repeatedly within our own timeout to allow
+    # the full time for all the expected messages to be published and pulled
+    while len(received_messages) < expected_msg_count and not time.time() > deadline:
+        try:
+            response = subscriber.pull(subscription_path, max_messages=expected_msg_count, timeout=1)
+        except DeadlineExceeded:
+            continue
+        if response.received_messages:
+            subscriber.acknowledge(subscription_path, [message.ack_id for message in response.received_messages])
+            received_messages.extend(response.received_messages)
+
+    test_helper.assertEqual(len(received_messages), expected_msg_count,
+                            f'Expected to pull exactly {expected_msg_count} message(s) from '
+                            f'subscription {subscription_path} but found {len(received_messages)} '
+                            f'within the {timeout} second timeout')
+    return received_messages
+
+
+def get_exact_number_of_pubsub_messages(subscription, expected_msg_count, timeout=30):
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = subscriber.subscription_path(Config.PUBSUB_PROJECT, subscription)
+    received_messages = _pull_exact_number_of_messages(subscriber, subscription_path, expected_msg_count, timeout)
+    parsed_message_bodies = []
+
+    for received_message in received_messages:
+        parsed_body = json.loads(received_message.message.data)
+        parsed_message_bodies.append(parsed_body)
+
+    subscriber.close()
+    return parsed_message_bodies
