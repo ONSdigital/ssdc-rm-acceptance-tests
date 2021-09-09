a4cc1a6f54f5a8c85b77fc6f702f3789b0b3747b
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2021-09-09 10:55:39 +0100

    Record user who quarantined message for audit trail (#43)

--- acceptance_tests/utilities/exception_manager_helper.py
+++ acceptance_tests/utilities/exception_manager_helper.py
@@ -1,11 +1,18 @@
 import time
 import requests
+
+from acceptance_tests.utilities.audit_trail_helper import get_unique_user_email
 from config import Config
 
 
 def quarantine_bad_messages(bad_message_hashes):
     for message_hash in bad_message_hashes:
-        response = requests.get(f"{Config.EXCEPTION_MANAGER_URL}/skipmessage/{message_hash}")
+        skip_request = {
+            "messageHash": message_hash,
+            "skippingUser": get_unique_user_email()
+        }
+
+        response = requests.post(f"{Config.EXCEPTION_MANAGER_URL}/skipmessage", json=skip_request)
         response.raise_for_status()
 
     time.sleep(3)
