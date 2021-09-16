4540f3fa6e860a058e26f789be8e70b40f15a899
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2021-09-16 12:37:22 +0100

    make all use email suffix function

--- acceptance_tests/features/steps/deactivate_uac.py
+++ acceptance_tests/features/steps/deactivate_uac.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 from behave import step
 
-from acceptance_tests.utilities.audit_trail_helper import get_random_alpha_numerics
+from acceptance_tests.utilities.audit_trail_helper import get_random_alpha_numerics, add_random_suffix_to_email
 from acceptance_tests.utilities.pubsub_helper import publish_to_pubsub
 from config import Config
 
@@ -13,7 +13,7 @@ from config import Config
 @step('a deactivate uac message is put on the queue with email_address "{email_address}"')
 def put_deactivate_uac_on_topic(context, email_address):
     context.correlation_id = str(uuid.uuid4())
-    context.originating_user = f'{email_address}@{get_random_alpha_numerics(4)}'
+    context.originating_user = add_random_suffix_to_email(email_address)
 
     message = _send_deactivate_uac_message(context.correlation_id, context.originating_user,
                                            context.emitted_uacs[0]['qid'])
@@ -22,8 +22,7 @@ def put_deactivate_uac_on_topic(context, email_address):
 
 @step('a bad deactivate uac message is put on the topic with email address "{email_address}"')
 def a_bad_deactivate_uac_message_is_put_on_the_topic(context, email_address):
-    message = _send_deactivate_uac_message(str(uuid.uuid4()), f'{email_address}@{get_random_alpha_numerics(4)}',
-                                           "123456789")
+    message = _send_deactivate_uac_message(str(uuid.uuid4()), add_random_suffix_to_email(email_address), "123456789")
     context.message_hashes = [hashlib.sha256(message.encode('utf-8')).hexdigest()]
 
 
