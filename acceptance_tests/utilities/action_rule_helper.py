5ba3a8170996ad95a734cc2067ac67a9727c0232
Ryan Grundy <ryan.grundy@ons.gov.uk>
2021-09-22 12:19:18 +0100

    Fixing action rules

--- acceptance_tests/utilities/action_rule_helper.py
+++ acceptance_tests/utilities/action_rule_helper.py
@@ -1,4 +1,3 @@
-import uuid
 from datetime import datetime
 
 import requests
@@ -9,13 +8,11 @@ from config import Config
 def create_print_action_rule(collex_id, classifiers, pack_code):
     url = f'{Config.SUPPORT_TOOL_API}/actionRules'
     body = {
-        'id': str(uuid.uuid4()),
         'type': 'PRINT',
         'packCode': pack_code,
         'triggerDateTime': f'{datetime.utcnow().isoformat()}Z',
-        'hasTriggered': False,
         'classifiers': classifiers,
-        'collectionExercise': 'collectionExercises/' + collex_id
+        'collectionExerciseId': collex_id
     }
 
     response = requests.post(url, json=body)
@@ -26,15 +23,11 @@ def create_print_action_rule(collex_id, classifiers, pack_code):
 def setup_deactivate_uac_action_rule(collex_id):
     url = f'{Config.SUPPORT_TOOL_API}/actionRules'
     body = {
-        'id': str(uuid.uuid4()),
         'type': 'DEACTIVATE_UAC',
         'packCode': None,
         'triggerDateTime': f'{datetime.utcnow().isoformat()}Z',
-        'hasTriggered': False,
         'classifiers': '',
-        'template': None,
-        'printSupplier': None,
-        'collectionExercise': 'collectionExercises/' + collex_id
+        'collectionExerciseId': collex_id
     }
 
     response = requests.post(url, json=body)
