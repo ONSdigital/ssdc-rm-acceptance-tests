dec0a770e544e72dfbb5e71a81153c96fc193850
Hugh Brace <hugh.ons@gmail.com>
2021-09-29 11:27:47 +0100

    AT for new case

---
+++ acceptance_tests/features/steps/new_case.py
@@ -0,0 +1,54 @@
+import json
+import uuid
+
+from behave import step
+
+from acceptance_tests.utilities.collex_helper import add_collex
+from acceptance_tests.utilities.pubsub_helper import publish_to_pubsub
+from acceptance_tests.utilities.survey_helper import add_sis2_survey
+from config import Config
+
+
+@step("a newCase event is built and submitted")
+def build_new_case_and_submit(context):
+    context.case_id = str(uuid.uuid4())
+    message = json.dumps(
+        {
+            "header": {
+                "version": Config.EVENT_SCHEMA_VERSION,
+                "topic": Config.PUBSUB_NEW_CASE_TOPIC,
+                "source": "cupidatat",
+                "channel": "EQ",
+                "dateTime": "1970-01-01T00:00:00.000Z",
+                "messageId": context.case_id,
+                "correlationId": context.case_id,
+                "originatingUser": "foo.bar@ons.gov.uk"
+            },
+            "payload": {
+                "newCase": {
+                    "caseId": context.case_id,
+                    "collectionExerciseId": context.collex_id,
+                    "sample": {
+                        "schoolId": "abc123",
+                        "schoolName": "Chesterthorps High School",
+                        "consentGivenTest": "true",
+                        "consentGivenSurvey": "true"
+                    },
+                    "sampleSensitive": {
+                        "firstName": "Fred",
+                        "lastName": "Bloggs",
+                        "childFirstName": "Jo",
+                        "childMiddleNames": "Rose May",
+                        "childLastName": "Pinker",
+                        "childDob": "2001-12-31",
+                        "additionalInfo": "Class 2A",
+                        "childMobileNumber": "07123456789",
+                        "childEmailAddress": "jo.rose.may.pinker@domain.com",
+                        "parentMobileNumber": "07123456789",
+                        "parentEmailAddress": "fred.bloggs@domain.com"
+                    }
+                }
+            }
+        }
+    )
+    publish_to_pubsub(message, project=Config.PUBSUB_PROJECT, topic=Config.PUBSUB_NEW_CASE_TOPIC)
--- acceptance_tests/utilities/survey_helper.py
+++ acceptance_tests/utilities/survey_helper.py
@@ -21,3 +21,21 @@ def add_survey(sample_validation_rules, sample_has_header_row=True, sample_file_
     response = requests.post(url, json=body)
     response.raise_for_status()
     return survey_id
+
+
+def add_sis2_survey():
+    survey_id = str(uuid.uuid4())
+    survey_name = 'test survey ' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    # sample_validation_rules =
+
+    url = f'{Config.SUPPORT_TOOL_API}/surveys'
+
+    body = {"id": survey_id,
+            "name": survey_name,
+            "sampleValidationRules": sample_validation_rules,
+            "sampleWithHeaderRow": sample_has_header_row,
+            "sampleSeparator": sample_file_separator}
+
+    response = requests.post(url, json=body)
+    response.raise_for_status()
+    return survey_id
