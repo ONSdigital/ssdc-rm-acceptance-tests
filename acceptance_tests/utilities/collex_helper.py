c767a8f8639ddcaa1043fbaf87bdc9db2944b414
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-09-30 09:54:03 +0100

    Fix broken ATs

--- acceptance_tests/utilities/collex_helper.py
+++ acceptance_tests/utilities/collex_helper.py
@@ -1,4 +1,3 @@
-import uuid
 from datetime import datetime
 
 import requests
@@ -7,11 +6,13 @@ from config import Config
 
 
 def add_collex(survey_id):
-    collex_id = str(uuid.uuid4())
     collex_name = 'test collex ' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
     url = f'{Config.SUPPORT_TOOL_API}/collectionExercises'
-    body = {'id': collex_id, 'name': collex_name, 'survey': 'surveys/' + survey_id}
+    body = {'name': collex_name, 'surveyId': survey_id}
     response = requests.post(url, json=body)
     response.raise_for_status()
+
+    collex_id = response.json()
+
     return collex_id
