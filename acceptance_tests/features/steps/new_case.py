9e06a989fc249f8032290cb7bf5c4da78a26d94f
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-10-11 12:30:26 +0100

    Fix ATs broken by newCase strict sensitive data checking

--- acceptance_tests/features/steps/new_case.py
+++ acceptance_tests/features/steps/new_case.py
@@ -33,8 +33,6 @@ def build_new_case_and_submit(context):
                     "sample": {
                         "schoolId": "abc123",
                         "schoolName": "Chesterthorps High School",
-                        "consentGivenTest": "true",
-                        "consentGivenSurvey": "true"
                     },
                     "sampleSensitive": {
                         "firstName": "Fred",
@@ -43,11 +41,10 @@ def build_new_case_and_submit(context):
                         "childMiddleNames": "Rose May",
                         "childLastName": "Pinker",
                         "childDob": "2001-12-31",
-                        "additionalInfo": "Class 2A",
-                        "childMobileNumber": "07123456789",
-                        "childEmailAddress": "jo.rose.may.pinker@domain.com",
-                        "parentMobileNumber": "07123456789",
-                        "parentEmailAddress": "fred.bloggs@domain.com"
+                        "mobileNumber": "07123456789",
+                        "emailAddress": "fred.bloggs@domain.com",
+                        "consentGivenTest": "true",
+                        "consentGivenSurvey": "true"
                     }
                 }
             }
@@ -62,8 +59,6 @@ def build_invalid_case_and_submit(context):
     context.message_id = str(uuid.uuid4())
     context.correlation_id = str(uuid.uuid4())
     context.originating_user = "foo.bar@ons.gov.uk"
-
-    # Should fail validation due to the schoolId number being > 11 chars
     message = json.dumps(
         {
             "header": {
@@ -83,8 +78,6 @@ def build_invalid_case_and_submit(context):
                     "sample": {
                         "schoolId": "schoolidistoolong",
                         "schoolName": "Chesterthorps High School",
-                        "consentGivenTest": "true",
-                        "consentGivenSurvey": "true"
                     },
                     "sampleSensitive": {
                         "firstName": "Fred",
@@ -93,11 +86,59 @@ def build_invalid_case_and_submit(context):
                         "childMiddleNames": "Rose May",
                         "childLastName": "Pinker",
                         "childDob": "2001-12-31",
-                        "additionalInfo": "Class 2A",
-                        "childMobileNumber": "07123456789",
-                        "childEmailAddress": "jo.rose.may.pinker@domain.com",
-                        "parentMobileNumber": "07123456789",
-                        "parentEmailAddress": "fred.bloggs@domain.com"
+                        "mobileNumber": "07123456789",
+                        "emailAddress": "fred.bloggs@domain.com",
+                        "consentGivenTest": "true",
+                        "consentGivenSurvey": "true"
+                    }
+                }
+            }
+        }
+    )
+    publish_to_pubsub(message, project=Config.PUBSUB_PROJECT, topic=Config.PUBSUB_NEW_CASE_TOPIC)
+
+    context.message_hashes = [hashlib.sha256(message.encode('utf-8')).hexdigest()]
+    context.sent_messages.append(message)
+
+
+@step("an invalid newCase event with extra sensitive data is put on the topic")
+def build_invalid_case_with_extra_sensitive_data_and_submit(context):
+    context.case_id = str(uuid.uuid4())
+    context.message_id = str(uuid.uuid4())
+    context.correlation_id = str(uuid.uuid4())
+    context.originating_user = "foo.bar@ons.gov.uk"
+    message = json.dumps(
+        {
+            "header": {
+                "version": Config.EVENT_SCHEMA_VERSION,
+                "topic": Config.PUBSUB_NEW_CASE_TOPIC,
+                "source": "cupidatat",
+                "channel": "EQ",
+                "dateTime": "1970-01-01T00:00:00.000Z",
+                "messageId": context.message_id,
+                "correlationId": context.correlation_id,
+                "originatingUser": context.originating_user
+            },
+            "payload": {
+                "newCase": {
+                    "caseId": context.case_id,
+                    "collectionExerciseId": context.collex_id,
+                    "sample": {
+                        "schoolId": "abc123",
+                        "schoolName": "Chesterthorps High School",
+                    },
+                    "sampleSensitive": {
+                        "bankAccountPinNumber": "1234 this is obviously data we wouldn't want to store!!",
+                        "firstName": "Fred",
+                        "lastName": "Bloggs",
+                        "childFirstName": "Jo",
+                        "childMiddleNames": "Rose May",
+                        "childLastName": "Pinker",
+                        "childDob": "2001-12-31",
+                        "mobileNumber": "07123456789",
+                        "emailAddress": "fred.bloggs@domain.com",
+                        "consentGivenTest": "true",
+                        "consentGivenSurvey": "true"
                     }
                 }
             }
