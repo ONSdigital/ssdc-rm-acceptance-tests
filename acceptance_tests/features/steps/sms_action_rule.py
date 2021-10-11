dc1916d99566da8553d8ec191ecc38ce9975be8e
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2021-10-11 12:48:35 +0100

    Fix ATs broken by newCase strict sensitive data checking (#62)

--- acceptance_tests/features/steps/sms_action_rule.py
+++ acceptance_tests/features/steps/sms_action_rule.py
@@ -3,11 +3,13 @@ from acceptance_tests.utilities.notify_helper import check_notify_api_called_wit
 from acceptance_tests.utilities.test_case_helper import test_helper
 
 
-@step('notify api was called with SMS template with phone number "{expected_phone_number}" and Childs name "{'
-      'expected_childs_name}"')
-def check_notify_called_with_correct_phone_number_and_childs_name(context, expected_phone_number, expected_childs_name):
+@step('notify api was called with SMS template with phone number "{expected_phone_number}" and child surname "{'
+      'expected_child_surname}"')
+def check_notify_called_with_correct_phone_number_and_child_surname(context, expected_phone_number,
+                                                                    expected_child_surname):
     received_notify_call = check_notify_api_called_with_correct_notify_template_id(expected_phone_number,
                                                                                    context.notify_template_id)
 
-    test_helper.assertEqual(received_notify_call['personalisation']['__sensitive__.CHILD_NAME'], expected_childs_name,
-                            f"Incorrect Childs name sent to notify, json sent {received_notify_call}")
+    test_helper.assertEqual(received_notify_call['personalisation']['__sensitive__.childLastName'],
+                            expected_child_surname,
+                            f"Incorrect child surname name sent to notify, json sent {received_notify_call}")
