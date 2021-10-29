5636c201122d5cc82d164422b8e82f93041e05ac
AdamHawtin <adam.hawtin@ons.gov.uk>
2021-10-29 12:52:21 +0100

    First stab at pubsub match and ack subscriber

--- acceptance_tests/utilities/survey_helper.py
+++ acceptance_tests/utilities/survey_helper.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from functools import partial
+from typing import Mapping
 
 import requests
 
-from acceptance_tests.utilities.event_helper import get_emitted_survey_update
+from acceptance_tests.utilities.pubsub_helper import get_matching_pubsub_messages_acking_others
 from acceptance_tests.utilities.test_case_helper import test_helper
 from config import Config
 
@@ -24,7 +26,7 @@ def add_survey(sample_validation_rules, sample_has_header_row=True, sample_file_
 
     survey_id = response.json()
 
-    survey_update_event = get_emitted_survey_update()
+    survey_update_event = get_emitted_survey_update(survey_name)
     test_helper.assertEqual(survey_update_event['name'], survey_name,
                             'Unexpected survey name')
     test_helper.assertEqual(survey_update_event['sampleDefinitionUrl'], "http://foo.bar",
@@ -33,3 +35,19 @@ def add_survey(sample_validation_rules, sample_has_header_row=True, sample_file_
                             'Unexpected metadata')
 
     return survey_id
+
+
+def get_emitted_survey_update(expected_survey_name):
+    message_received = get_matching_pubsub_messages_acking_others(Config.PUBSUB_OUTBOUND_SURVEY_SUBSCRIPTION,
+                                                                  partial(match_message_survey_name,
+                                                                          expected_survey_name=expected_survey_name))
+
+    return message_received['payload']['surveyUpdate']
+
+
+def match_message_survey_name(message: Mapping, expected_survey_name=None) -> (bool, str):
+    """Survey update event survey name matches expected"""
+    if message['payload']['surveyUpdate']['name'] == expected_survey_name:
+        return True, ''
+    return False, f'Actual survey name "{message["payload"]["surveyUpdate"]["name"]}" ' \
+                  f'does not match expected "{expected_survey_name}"'
