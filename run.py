9ae71104daf0581a9afb2e6e304de27a929cd1d8
AdamHawtin <adam.hawtin@ons.gov.uk>
2022-03-31 14:23:58 +0100

    Fix tags and target stuff

--- run.py
+++ run.py
@@ -6,7 +6,6 @@ from behave import __main__ as behave_executable
 DEFAULT_LOG_LEVEL = 'INFO'
 DEFAULT_BEHAVE_FORMAT = 'pretty'
 DEFAULT_FEATURE_DIRECTORY = 'acceptance_tests/features'
-DEFAULT_TAGS = '~@regression'
 
 
 def parse_arguments():
@@ -18,7 +17,7 @@ def parse_arguments():
     parser.add_argument('--log_level', '-l', help='Logging level', default=DEFAULT_LOG_LEVEL)
     parser.add_argument('--format', '-f', help='Behave format', default=DEFAULT_BEHAVE_FORMAT)
     parser.add_argument('--feature_directory', '-fd', help='Feature directory', default=DEFAULT_FEATURE_DIRECTORY)
-    parser.add_argument('--tags', '-t', help='Tags', default=DEFAULT_TAGS)
+    parser.add_argument('--tags', '-t', help='Tags')
     parser.add_argument('--show_skipped', help='Show skipped tests', action='store_true')
 
     return parser.parse_args()
