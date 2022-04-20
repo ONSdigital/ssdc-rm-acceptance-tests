9ec0f432e28d7f71b62cbf72d5d40fe174d0015b
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-04-20 09:57:04 +0100

    add tags to ignore @rh

--- acceptance_tests/features/RM_To_RH.feature
+++ acceptance_tests/features/RM_To_RH.feature
@@ -1,5 +1,6 @@
 Feature: Loading a file in RM, results in data in RH
 
+  @rh
   Scenario: Load a file in RM, poll RH
     Given sample file "social_sample_3_lines_fields.csv" is loaded successfully for survey type "social"
     When the rh-service api is polled for attribute "ADDRESS_LINE1" and value "Something View" the correct case is returned
--- run.py
+++ run.py
@@ -32,10 +32,23 @@ def main():
     logging.basicConfig(level=args.log_level)
 
     show_skipped = '--show-skipped' if args.show_skipped else '--no-skipped'
-    tags_arg = f'--tags {args.tags}' if args.tags else ''
+
+    tags_arg = parse_tags(args.tags)
+
     args = f'--logging-level {args.log_level} --format {args.format} {args.feature_directory} {tags_arg} {show_skipped}'
 
     return behave_executable.main(args)
+
+
+# --tags=~@tag1,~@tag2 doesn't work.  Needs to be --tags @tag1 --tags ~@tag2
+def parse_tags(tags_arg):
+    new_tag_arg = ''
+
+    for tag in tags_arg.split(","):
+        new_tag_arg += f' --tags {tag}'
+
+    print(new_tag_arg)
+    return new_tag_arg
 
 
 if __name__ == '__main__':
