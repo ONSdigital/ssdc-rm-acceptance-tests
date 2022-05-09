4ea8ae51b933afa07228d3fc0aef4c94351a8659
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-05-09 14:26:25 +0100

    Fix add_survey signature

--- acceptance_tests/utilities/survey_helper.py
+++ acceptance_tests/utilities/survey_helper.py
@@ -9,8 +9,8 @@ from acceptance_tests.utilities.test_case_helper import test_helper
 from config import Config
 
 
-def add_survey(sample_validation_rules, sample_has_header_row=True, sample_file_separator=',',
-               scheduleTemplate=None, sample_definition_url="http://foo.bar.json"):
+def add_survey(sample_validation_rules, sample_definition_url="http://foo.bar.json", sample_has_header_row=True,
+               sample_file_separator=',', schedule_template=None):
     survey_name = 'test survey ' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
     url = f'{Config.SUPPORT_TOOL_API}/surveys'
@@ -21,7 +21,7 @@ def add_survey(sample_validation_rules, sample_has_header_row=True, sample_file_
             "sampleSeparator": sample_file_separator,
             "sampleDefinitionUrl": sample_definition_url,
             "metadata": {'foo': 'bar'},
-            "scheduleTemplate": scheduleTemplate}
+            "scheduleTemplate": schedule_template}
 
     response = requests.post(url, json=body)
     response.raise_for_status()
@@ -31,7 +31,7 @@ def add_survey(sample_validation_rules, sample_has_header_row=True, sample_file_
     survey_update_event = get_emitted_survey_update(survey_name)
     test_helper.assertEqual(survey_update_event['name'], survey_name,
                             'Unexpected survey name')
-    test_helper.assertEqual(survey_update_event['sampleDefinitionUrl'], "http://foo.bar",
+    test_helper.assertEqual(survey_update_event['sampleDefinitionUrl'], sample_definition_url,
                             'Unexpected sample definition URL')
     test_helper.assertEqual(survey_update_event['metadata'], {'foo': 'bar'},
                             'Unexpected metadata')
