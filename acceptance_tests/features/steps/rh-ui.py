19e23d7765c307c1b0f22ec62be21794faf05882
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-07-15 10:34:19 +0100

    noddy example navigation and test check

--- acceptance_tests/features/steps/rh-ui.py
+++ acceptance_tests/features/steps/rh-ui.py
@@ -7,13 +7,11 @@ use_step_matcher("re")
 @given("I've opened RH-UI")
 def open_rh_ui(context):
     browser = Browser()  # defaults to firefox
-    browser.visit('http://google.com')
-    browser.fill('q', 'splinter - python acceptance testing for web applications')
-    browser.find_by_name('btnK').click()
+    browser.visit('https://stackoverflow.com/questions/39488311/python-reproduce-splinter-selenium-behaviour-for-testing-a-website-that-uses-j')
 
-    if browser.is_text_present('splinter.readthedocs.io'):
-        print("Yes, the official website was found!")
+    if browser.is_text_present('class ScreenShotListener(AbstractEventListener'):
+        print("good")
     else:
-        print("No, it wasn't found... We need to improve our SEO techniques")
+        print("bad")
 
     browser.quit()
