bf6e740064762040db54e5a088524282d77dbd7d
liamtoozer <liam.toozer@gmail.com>
2022-07-20 16:12:23 +0100

    Update dev convenience script to use HTTTP proxy

--- run_gke.sh
+++ run_gke.sh
@@ -13,10 +13,10 @@ else
 fi
 
 if [ "$NAMESPACE" ]; then
-    kubectl config set-context $(kubectl config current-context) --namespace=$NAMESPACE
+    HTTPS_PROXY=localhost:8888 kubectl config set-context $(HTTPS_PROXY=localhost:8888 kubectl config current-context) --namespace=$NAMESPACE
     echo "NAMESPACE = [$NAMESPACE] Set kubectl namespace for subsequent commands [$NAMESPACE]."
 fi
-echo "Running RM Acceptance Tests [$(kubectl config current-context)]..."
+echo "Running RM Acceptance Tests [$(HTTPS_PROXY=localhost:8888 kubectl config current-context)]..."
 
 
 BEHAVE_TAGS = ''
@@ -28,12 +28,12 @@ else
 fi
 
 
-kubectl delete pod acceptance-tests --wait || true
+HTTPS_PROXY=localhost:8888 kubectl delete pod acceptance-tests --wait || true
 
-kubectl apply -f acceptance_tests_pod.yml
+HTTPS_PROXY=localhost:8888 kubectl apply -f acceptance_tests_pod.yml
 
-kubectl wait --for=condition=Ready pod/acceptance-tests --timeout=200s
+HTTPS_PROXY=localhost:8888 kubectl wait --for=condition=Ready pod/acceptance-tests --timeout=200s
 
-kubectl exec -it acceptance-tests -- /bin/bash -c "sleep 2; behave acceptance_tests/features $BEHAVE_TAGS --logging-level WARN"
+HTTPS_PROXY=localhost:8888 kubectl exec -it acceptance-tests -- /bin/bash -c "sleep 2; behave acceptance_tests/features $BEHAVE_TAGS --logging-level WARN"
 
-kubectl delete pod acceptance-tests || true
+HTTPS_PROXY=localhost:8888 kubectl delete pod acceptance-tests || true
