025121cf0501927ec51f8ae1315a484dce145f42
Ryan Grundy <ryan.grundy@ons.gov.uk>
2022-08-18 14:35:51 +0100

    Added more to the test

--- acceptance_tests/features/SupportTool_UI.feature
+++ acceptance_tests/features/SupportTool_UI.feature
@@ -4,7 +4,11 @@ Feature: Test basic Support Tool Functionality
   Scenario: Create a Survey, Collection Exercise and Load a Sample
     Given the support tool landing page is displayed
     And The Create Survey Button is clicked on
-    When a Survey called "CreateSurveyTest" plus unique suffix is created for sample file "social_sample_3_lines_fields.csv"
-    
+    And a Survey called "CreateSurveyTest" plus unique suffix is created for sample file "social_sample_3_lines_fields.csv"
+    When the survey is clicked on it should display the collex page
+    And the create collection exercise button is clicked on and entered in details
+    And the collex is clicked on and displays the details page
+    And I click the upload sample file button with file "social_sample_3_lines_fields.csv"
+
 
 #  Do all of the above in a single step too for other tests
--- acceptance_tests/features/steps/supporttool_ui.py
+++ acceptance_tests/features/steps/supporttool_ui.py
@@ -1,5 +1,6 @@
 import time
 import json
+from datetime import datetime
 
 from behave import step
 
@@ -38,3 +39,53 @@ def create_survey_in_UI(context, survey_prefix, sample_file_name):
 
     test_helper.assertEquals(
         len(context.browser.find_by_id('surveyListTable').first.find_by_text(context.survey_name)), 1)
+
+
+@step('the survey is clicked on it should display the collex page')
+def click_into_collex_page(context):
+    context.browser.find_by_id('surveyListTable').first.find_by_text(context.survey_name).click()
+
+
+@step('the create collection exercise button is clicked on and entered in details')
+def click_create_collex_button(context):
+    context.browser.find_by_id('createCollectionExerciseBtn').click()
+    context.collex_name = 'test collex ' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+    context.browser.find_by_id('collectionExerciseNameTextField').fill(context.collex_name)
+
+    context.expected_collection_instrument_url = "http://test-eq.com/test-schema"
+    collection_instrument_selection_rules = [
+        {
+            "priority": 100,
+            "spelExpression": "caze.sample['POSTCODE'] == 'NW16 FNK'",
+            "collectionInstrumentUrl": context.expected_collection_instrument_url
+        },
+        {
+            "priority": 0,
+            "spelExpression": None,
+            "collectionInstrumentUrl": "this URL should not be chosen. If it is, the test is a failure"
+        }
+    ]
+    context.browser.find_by_id('collectionExerciseReferenceTextField').fill('MVP012021')
+    context.browser.find_by_id('collectionExerciseCIRulesTextField').fill(json.dumps(collection_instrument_selection_rules))
+    context.browser.find_by_id('postCreateCollectionExerciseBtn').click()
+    test_helper.assertEquals(
+        len(context.browser.find_by_id('collectionExerciseTableList').first.find_by_text(context.collex_name)), 1)
+
+@step('the collex is clicked on and displays the details page')
+def click_into_collex_details(context):
+    context.browser.find_by_id('collectionExerciseTableList').first.find_by_text(context.collex_name).click()
+
+
+@step('I click the upload sample file button with file "{sample_file_name}"')
+def click_load_sample(context, sample_file_name):
+    Config.RESOURCE_FILE_PATH.joinpath('sample_files')
+    sample_file_path = SAMPLE_FILES_PATH.joinpath(sample_file_name)
+    context.browser.find_by_id('contained-button-file').first.type(str(sample_file_path))
+    context.browser.reload()
+    test_helper.assertEquals(
+        len(context.browser.find_by_id('sampleFilesList').first.find_by_text(sample_file_name)), 1)
+    print(context.browser.find_by_id('sampleFilesList').first.find_by_text(sample_file_name))
+
+
+
