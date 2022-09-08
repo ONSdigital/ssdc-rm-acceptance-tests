980ca26f7fd7467477fb25506cddf51da99f4b45
ryangrundy7 <35301276+ryangrundy7@users.noreply.github.com>
2022-09-08 14:57:40 +0100

    Added test for sdx receipt (#122)
    * Added test for sdx receipt
    
    * Using email fulfilment instead to speed up test
    
    * Changed event name

--- acceptance_tests/features/steps/receipt.py
+++ acceptance_tests/features/steps/receipt.py
@@ -18,12 +18,34 @@ def send_receipt(context):
     context.sent_messages.append(message)
 
 
+@step("a receipt message is published to the sdx pubsub receipting topic")
+def send_sdx_receipt(context):
+    # For the SDX receipt we do not set the correlation id so we need to make sure it's not set for any future checks
+    context.correlation_id = None
+    message = _send_sdx_receipt(context.emitted_uacs[0]['qid'])
+    context.sent_messages.append(message)
+
+
 @step('a bad receipt message is put on the topic')
 def a_bad_receipt_message_is_put_on_the_topic(context):
     context.originating_user = add_random_suffix_to_email(context.scenario_name)
     message = _send_receipt_message(str(uuid.uuid4()), context.originating_user, "987654321")
     context.message_hashes = [hashlib.sha256(message.encode('utf-8')).hexdigest()]
     context.sent_messages.append(message)
+
+
+def _send_sdx_receipt(qid):
+    message = json.dumps({
+        "data": {
+            "questionnaire_id": qid,
+            "source": "SRM"
+        }}
+    )
+
+    publish_to_pubsub(message,
+                      Config.PUBSUB_PROJECT,
+                      'sdx_receipt', content_type="application/json")
+    return message
 
 
 def _send_receipt_message(correlation_id, originating_user, qid):
