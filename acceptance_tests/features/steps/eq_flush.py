00e67a83f311429a07684e47e629d1a37ec0b3f1
AdamHawtin <adam.hawtin@ons.gov.uk>
2023-01-10 13:19:43 +0000

    Improve eq stub functions

--- acceptance_tests/features/steps/eq_flush.py
+++ acceptance_tests/features/steps/eq_flush.py
@@ -3,6 +3,7 @@ from typing import Mapping
 
 import requests
 from behave import step
+from tenacity import retry, wait_fixed, stop_after_delay
 
 from acceptance_tests.utilities.jwe_helper import decrypt_signed_jwe
 from acceptance_tests.utilities.pubsub_helper import get_matching_pubsub_message_acking_others
@@ -22,33 +23,29 @@ def check_eq_flush_cloud_task_message(context):
     cloud_task_message = get_matching_pubsub_message_acking_others(Config.PUBSUB_CLOUD_TASK_QUEUE_AT_SUBSCRIPTION,
                                                                    _message_matcher)
 
-    assert cloud_task_message['payload'].get('transactionId') is not None, \
-        'Expect EQ flush cloud task payload to have a transaction ID'
+    test_helper.assertIsNotNone(cloud_task_message['payload'].get('transactionId'),
+                                'Expected EQ flush cloud task payload to have a transaction ID')
 
 
 @step('the eQ flush endpoint is called with the token for flushing the correct QIDs partial')
 def check_eq_stub_flush_call_log(context):
-    timeout = time.time() + 30  # Allow time for the calls
-    while time.time() < timeout:
-        eq_stub_response = requests.get(f'{Config.EQ_FLUSH_STUB_URL}/log/flush')
-        eq_stub_response.raise_for_status()
-        eq_stub_log = eq_stub_response.json()
+    flush_call = poll_for_one_eq_stub_flush_call()
 
-        if len(eq_stub_log) > 1:
-            test_helper.fail(
-                f'Found multiple calls in the eq stub call log when only one is expected, call log: {eq_stub_log}')
-        if not eq_stub_log:
-            time.sleep(0.5)
-            continue
-        eq_stub_call = eq_stub_log[0]
-        flush_token = eq_stub_call['token']
-        break
-    else:
-        test_helper.fail('Timed out waiting for eq stub call')
-
-    token_contents = decrypt_signed_jwe(flush_token)
+    token_contents = decrypt_signed_jwe(flush_call['token'])
 
     test_helper.assertTrue(token_contents['response_id'].startswith(context.emitted_uacs[0]['qid']),
                            'Flush response ID should start with the correct QID')
     test_helper.assertIsNotNone(token_contents['tx_id'], 'tx_id must be set in the flushing claims')
     test_helper.assertEqual(token_contents['roles'], ['flusher'], 'The roles must be "flusher" in flushing claims')
+
+
+@retry(wait=wait_fixed(1), stop=stop_after_delay(30))
+def poll_for_one_eq_stub_flush_call() -> Mapping:
+    eq_stub_response = requests.get(f'{Config.EQ_FLUSH_STUB_URL}/log/flush')
+    eq_stub_response.raise_for_status()
+    eq_stub_log = eq_stub_response.json()
+
+    test_helper.assertEqual(len(eq_stub_log), 1,
+                            f'Expected exactly one EQ flush call to be logged, log: {eq_stub_log}')
+
+    return eq_stub_log[0]
