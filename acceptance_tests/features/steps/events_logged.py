0ccb44fd5756b52af0af14cea36c893fc0ab94fd
AdamHawtin <adam.hawtin@ons.gov.uk>
2023-03-08 12:45:32 +0000

    Test for EXPORT_FILE events in export file ATs

--- acceptance_tests/features/steps/events_logged.py
+++ acceptance_tests/features/steps/events_logged.py
@@ -8,3 +8,9 @@ from acceptance_tests.utilities.case_api_helper import check_if_event_list_is_ex
 @step("the events logged against the case are {expected_event_list:array}")
 def check_case_events_logged(context, expected_event_list: List):
     check_if_event_list_is_exact_match(expected_event_list, context.emitted_cases[0]['caseId'])
+
+
+@step("the events logged against the cases are {expected_event_list:array}")
+def check_cases_events_logged(context, expected_event_list: List):
+    for case in context.emitted_cases:
+        check_if_event_list_is_exact_match(expected_event_list, case['caseId'])
