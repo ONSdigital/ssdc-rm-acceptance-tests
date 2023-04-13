5976e93d5ff34684fdcbd220183e3de5d24bd0bf
Adam <35268982+AdamHawtin@users.noreply.github.com>
2023-04-13 12:57:50 +0100

    903 Update NIFI export notification file path (#167)

--- acceptance_tests/features/steps/nifi_notification.py
+++ acceptance_tests/features/steps/nifi_notification.py
@@ -9,8 +9,8 @@ from config import Config
 @step('a notification PubSub message is sent to NIFI with the correct export file details')
 def check_nifi_export_file_notification_message(context):
     def _message_matcher(message: Mapping) -> tuple[bool, str]:
-        return message['files'][0]['name'].startswith(
-            context.pack_code), 'Notification message file name prefix did not match the pack code'
+        return message['files'][0]['name'].startswith(f'internal_reprographics/print_services/{context.pack_code}'), \
+            'Notification message file name prefix did not match the pack code'
 
     get_matching_pubsub_message_acking_others(
         subscription=Config.PUBSUB_NIFI_INTERNAL_PRINT_NOTIFICATION_SUBSCRIPTION,
