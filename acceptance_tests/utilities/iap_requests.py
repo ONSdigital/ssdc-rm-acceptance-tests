eb7097b69fe86346105981886ac5ced2dbf00699
AdamHawtin <adam.hawtin@ons.gov.uk>
2024-04-05 12:04:56 +0100

    Small refactor to allow different IAP clients to be specified

--- acceptance_tests/utilities/iap_requests.py
+++ acceptance_tests/utilities/iap_requests.py
@@ -5,22 +5,24 @@ from google.oauth2 import id_token
 from config import Config
 
 
-def make_request(method: str = "GET", url: str = None, **kwargs) -> requests.Response:
-    """Make an IAP authorized request if IAP config is present,
-    otherwise fall back on a regular, unauthenticated request
+def make_request(method: str = "GET", url: str = None,
+                 iap_client_id: str = Config.SUPPORT_TOOL_IAP_CLIENT_ID, **kwargs) -> requests.Response:
+    """Make an IAP authorized request if IAP config is present
     Kwargs:
       method: The request method to use
               ('GET', 'OPTIONS', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE')
       url: The Identity-Aware Proxy-protected URL to fetch.
+      iap_client_id: (Optional) The IAP Client ID to use for authentication. If not configured, it will fall back on
+                     a regular, unauthenticated request. Defaults to the configured Support Tool Client if present.
       **kwargs: Any of the parameters defined for the request function:
                 https://github.com/requests/requests/blob/master/requests/api.py
     """
-    if Config.SUPPORT_TOOL_IAP_CLIENT_ID:
+    if iap_client_id:
         return _make_iap_request(method, url, **kwargs)
     return requests.request(method, url, **kwargs)
 
 
-def _make_iap_request(method: str = 'GET', url: str = None, **kwargs) -> requests.Response:
+def _make_iap_request(method: str = 'GET', url: str = None, iap_client_id: str = None, **kwargs) -> requests.Response:
     """Makes a request to an application protected by Identity-Aware Proxy.
     Args:
       method: The request method to use
@@ -38,7 +40,7 @@ def _make_iap_request(method: str = 'GET', url: str = None, **kwargs) -> request
         kwargs['timeout'] = 90
 
     # Obtain an OpenID Connect (OIDC) token from metadata server or using service account.
-    open_id_connect_token = id_token.fetch_id_token(Request(), Config.SUPPORT_TOOL_IAP_CLIENT_ID)
+    open_id_connect_token = id_token.fetch_id_token(Request(), iap_client_id)
 
     # Initialise headers if none are passed in the kwargs
     if 'headers' not in kwargs:
@@ -48,5 +50,4 @@ def _make_iap_request(method: str = 'GET', url: str = None, **kwargs) -> request
     # Google-issued OpenID Connect token for the service account.
     kwargs['headers']['Proxy-Authorization'] = f'Bearer {open_id_connect_token}'
 
-    return requests.request(
-        method, url, **kwargs)
+    return requests.request(method, url, **kwargs)
