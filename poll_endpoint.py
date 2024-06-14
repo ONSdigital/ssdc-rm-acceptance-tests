cf8611c873f6d581189bae0e6fdfb2b47b503423
Ryan Grundy <ryan.grundy@ons.gov.uk>
2024-06-14 11:46:27 +0100

    Added sleep interval and changed how the loop works

--- poll_endpoint.py
+++ poll_endpoint.py
@@ -6,22 +6,18 @@ from requests.exceptions import ConnectionError, HTTPError
 from acceptance_tests.utilities.iap_requests import make_request
 
 
-def poll_endpoint(url: str, max_retries: int):
-    check_endpoint = True
-    num_of_retries = 0
-    while check_endpoint:
+def poll_endpoint(url: str, max_retries: int, sleep_interval: int):
+    for _attempts in range(max_retries):
         try:
             response = make_request(url=url)
             response.raise_for_status()
-            check_endpoint = False
+            print(f"Able to poll {url} endpoint. Exiting script")
+            return
         except (ConnectionError, HTTPError):
-            num_of_retries += 1
-            if num_of_retries == max_retries:
-                print(f"Reached maximum number of retries on {url}. Stopping script")
-                exit(1)
-
-            print(f"{url} is not available. Sleeping for a minute")
-            sleep(60)
+            print(f"{url} is not available. Sleeping for {sleep_interval} second(s)")
+            sleep(sleep_interval)
+    print(f"Reached maximum number of retries on {url}. Stopping script")
+    exit(1)
 
 
 def parse_arguments():
@@ -31,13 +27,16 @@ def parse_arguments():
     parser.add_argument('--url', help='The url you want to poll', type=str, required=True)
     parser.add_argument('--max_retries', help='Maximum number of retries we want to check URL is up', type=int,
                         required=True)
+    parser.add_argument('--sleep_interval', help='OPTIONAL: How long we want to sleep between retries in seconds',
+                        type=int,
+                        default=60)
 
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
-    poll_endpoint(url=args.url, max_retries=args.max_retries)
+    poll_endpoint(url=args.url, max_retries=args.max_retries, sleep_interval=args.sleep_interval)
 
 
 if __name__ == "__main__":
