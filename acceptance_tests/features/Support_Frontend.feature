2568cf6795a430cd2ddb3c3df47dfd8503b437d4
Daniel Banks_ONS <daniel.banks@ons.gov.uk>
2025-09-08 14:37:30 +0100

    added support frontend survey unhappy features

--- acceptance_tests/features/Support_Frontend.feature
+++ acceptance_tests/features/Support_Frontend.feature
@@ -1,13 +1,13 @@
 @UI @SupportFrontend
 Feature: Test functionality of the Support Frontend
 
-  @regression
   Scenario: Create a Survey and View Details
     Given the support frontend is displayed
     When the "Create new survey" button is clicked
     And a survey called "SupportFrontendTest" plus unique suffix is created
     Then I should see the new surveys details
 
+  @regression
   Scenario: Edit a surveys details
     Given the support frontend is displayed
     And the "Create new survey" button is clicked
@@ -15,3 +15,25 @@ Feature: Test functionality of the Support Frontend
     When the name edit link is clicked
     And the name is changed to "EditedSupportFrontendTest"
     Then I should see the edited survey name
+
+  Scenario: Create an invalid survey
+    Given the support frontend is displayed
+    When the "Create new survey" button is clicked
+    And a survey with no filed entered is attempted to be created
+    Then I should see error messages
+
+  @regression
+  Scenario: Edit a surveys details and removing fields
+    Given the support frontend is displayed
+    And the "Create new survey" button is clicked
+    And a survey called "SupportFrontendTest" plus unique suffix is created
+    When the name edit link is clicked
+    And fields are emptied
+    Then I should see error messages other than for sample template
+
+  @regression
+  Scenario: Create a survey with a too long name
+    Given the support frontend is displayed
+    When the "Create new survey" button is clicked
+    And a survey with a name longer than 255 characters is attempted to be created
+    Then the name should be truncated to 255 characters
--- acceptance_tests/features/steps/support_frontend.py
+++ acceptance_tests/features/steps/support_frontend.py
@@ -18,10 +18,13 @@ def click_on_create_new_survey_button(context):
 @step('a survey called "{survey_name}" plus unique suffix is created')
 def input_survey_details_and_save_survey(context, survey_name):
     context.survey_name = survey_name + get_random_alpha_numerics(5)
-    context.browser.find_by_id("name_input").fill(context.survey_name)
+    context.browser.find_by_id("name_input", wait_time=5).fill(context.survey_name)
     context.browser.find_by_id("sample_definition_url_input").fill(survey_name)
-    context.browser.find_by_id("sample_validation_rules_input").fill("[]")
-    context.browser.find_by_id("create-survey-button").click()
+    radios = context.browser.find_by_css("input[type='radio']")
+    radios[0].click()
+    button = context.browser.find_by_id("create-survey-button")
+    context.browser.execute_script("arguments[0].scrollIntoView(true);", button._element)
+    button.click()
 
 
 @step("I should see the new surveys details")
@@ -53,3 +56,58 @@ def find_edited_survey_name(context):
                             f"Expected survey name to be {context.edited_survey_name},"
                             f" but found {context.browser.find_by_id("name_value").first.text}")
     test_helper.assertNotEqual(context.survey_name, context.edited_survey_name, "The survey name was not edited")
+
+
+@step('a survey with no filed entered is attempted to be created')
+def create_survey_with_no_name(context):
+    context.browser.find_by_id("create-survey-button").click()
+
+
+@step('I should see error messages')
+def see_error_messages(context):
+    test_helper.assertEqual(context.browser.find_by_id("alert", wait_time=5).first.text,
+                            "There are 3 problems with this page", "No error summary shown")
+    test_helper.assertTrue(context.browser.is_text_present("Enter a survey name"), "No error message shown for name")
+    test_helper.assertTrue(context.browser.is_text_present("Enter a sample definition URL"),
+                           "No error message shown for sample definition URL")
+    test_helper.assertTrue(context.browser.is_text_present("Select a sample template"),
+                           "No error message shown for sample template")
+
+
+@step('fields are emptied')
+def empty_fields(context):
+    context.browser.find_by_id("name_input", wait_time=5).fill("")
+    context.browser.find_by_id("sample_definition_url_input").fill("")
+    radios = context.browser.find_by_css("input[type='radio']:checked")
+    if radios:
+        radios[0].click()
+    context.browser.find_by_id("create-survey-button").click()
+
+
+@step('I should see error messages other than for sample template')
+def see_error_messages_minus_template(context):
+    test_helper.assertEqual(context.browser.find_by_id("alert", wait_time=5).first.text,
+                            "There are 2 problems with this page", "No error summary shown")
+    test_helper.assertTrue(context.browser.is_text_present("Enter a survey name"),
+                           "No error message shown for name")
+    test_helper.assertTrue(context.browser.is_text_present("Enter a sample definition URL"),
+                           "No error message shown for sample definition URL")
+
+
+@step('a survey with a name longer than 255 characters is attempted to be created')
+def create_survey_with_long_name(context):
+    long_name = "This is a very long survey name that is definitely going to be longer than 255 characters. " \
+                "In fact, it is so long that it just keeps going and going and going and going and going and going " \
+                "and going and going and going and going and going and going and going and going and going and " \
+                "going and going and going and going and going and going and going and going and going and going " \
+                "and going and going and going and going and going and going and going!"
+    context.browser.find_by_id("name_input", wait_time=5).fill(long_name)
+    context.browser.find_by_id("sample_definition_url_input").fill("Test URL")
+    radios = context.browser.find_by_css("input[type='radio']")
+    radios[0].click()
+
+
+@step('the name should be truncated to 255 characters')
+def name_truncated_to_255_characters(context):
+    test_helper.assertEqual(len(context.browser.find_by_id("name_input").first.value), 255,
+                            "The survey name is not 255 characters long")
